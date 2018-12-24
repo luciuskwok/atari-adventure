7cc0c5fff4f5e3a1b7beb0f15176495290c18a75
Lucius Kwok <lk@felttip.com>
2018-12-24 02:35:50 -0500

    Changed bits_asm() to correctly pull bits off the right of the byte. Still not working correctly though.

--- puff_asm.s
+++ puff_asm.s
@@ -21,17 +21,28 @@ STATE_ENV = _puff_state+14
 .code
 
 .proc _bits_asm				; fastcall: "need" parameter is passed in A. 
-	tay						; use Y for need, which goes down to zero when complete
+	cmp #0
+	bne set_up_locals
+	ldx #0
+	rts   					; if "need" == 0, return 0
+
+set_up_locals:
+	pha 					; save "need" for later
+	tay						; use Y as decrementing index
 	lda #0					; set return value = 0
-	pha 					; use 2 bytes on stack to locally store the 16-bit return value
+	pha 					; up to 15 bytes are needed, so use 2 bytes on stack
 	pha
 	beq while_need 			; do while (need != 0)
 
-do_while:		
+do_need:		
 	ldx STATE_INCNT			; if STATE_INCNT == STATE_INLEN:
 	cpx STATE_INLEN 		;     error: out of input
+	bne load_bitbuf;
+	ldx STATE_INCNT+1		; must check both bytes of 16-bit values
+	cpx STATE_INLEN+1
 	beq error_jmp
 
+load_bitbuf:
 	ldx STATE_BITCNT		; if STATE_BITCNT == 0:
 	bne skip_load			;     load new byte into STATE_BITBUF	
 
@@ -44,36 +55,50 @@ do_while:
 	pha 					; push result of addition onto stack to be used as address
 	txa 
 	pha
-	tsx						; get the stack pointer, which is 1 byte below the previously pushed data
-	inx
-	lda $0100,X 			; load the byte at addr (STATE_IN[STATE_INT])
+	tsx						; get the stack pointer
+	lda $0101,X 			; load the next input byte (STATE_IN[STATE_INT])
 	sta STATE_BITBUF		; store the byte in STATE_BITBUF
 	lda #8
 	sta STATE_BITCNT		; set STATE_BITCNT = 8
 	pla						; pop the temp address off the stack
 	pla
 
-skip_load:
-	asl STATE_BITBUF		; shift left, bit 0 is set to 0, bit 7 is shifted into carry
-	pla 					; load LSB
-	rol a 					; rotate LSB left, carry into bit 0, bit 7 into carry
-	tax 					; store LSB in X so it can be pushed last
-	pla 					; load MSB
-	rol a 					; rotate MSB left, carry into bit 0, bit 7 into carry (which is ignored)
-	pha 					; push MSB
-	txa
-	pha 					; push LSB from X
+skip_load:					; DEFLATE format specifies that bits come off the right side
+	lsr STATE_BITBUF		; shift right, 0->(bit 7), (bit 0)->carry
+	tsx						; get the stack pointer
+	ror $0101,X 			; MSB: carry->(bit 7), (bit 0)->carry
+	ror $0102,X 			; LSB: carry->(bit 7), (bit 0)->carry(discarded)
 	dec STATE_BITCNT		; STATE_BITCNT -= 1
 	dey						; need -= 1
 while_need:
 	cpy #0					; while (need != 0)
-	bne do_while
+	bne do_need
 
-	pla						; pull LSB
-	tay 					; move LSB to Y temporarily 
-	pla						; pull MSB
-	tax 					; move MSB to X for return
-	tya 					; move LSB ot A for return
+; At this point the return value is on the stack as two bytes, but the bits are left-justified
+; and need to be right justified.
+	tsx 					; get the stack pointer
+	lda $0103,X 			; load "need" parameter (3rd byte on stack)
+	sec 					; set carry so it doesn't affect subtraction
+	sbc #16 				; Y = "need" - 16 = negative number of bit shifts
+	tay 					; use Y as index register to count number of bit shifts
+	clc
+	bcc while_bit_shifter	; always branch
+
+do_bit_shifter:
+	lsr $0101,X 			; MSB: 0->(bit 7), (bit 0)->carry
+	ror $0102,X 			; LSB: carry->(bit y), (bit 0)->carry(discarded)
+	iny 					; Y += 1
+while_bit_shifter:
+	cpy #0 					; while (Y != 0)
+	bne do_bit_shifter
+
+return:
+	pla	; MSB -> X
+	tax 
+	pla ; LSB -> Y
+	tay
+	pla ; discard to put stack pointer in correct place for return
+	txa ; MSB -> A
 	rts 					; return bits
 
 error_jmp:
