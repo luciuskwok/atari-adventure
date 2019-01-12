d120fbeba0ff4b1a9d6e9ef3cd6c7fb96f6162fc
Lucius Kwok <lk@felttip.com>
2019-01-12 05:37:41 -0500

    Rewrote decodeRunLenRange to use fewer zeropage variables.

--- map_.asm
+++ map_.asm
@@ -36,7 +36,7 @@
 	; RMARGN: width of map window 
 	.importzp sreg, ptr2 ; _setTileSprite uses ptr1
 
-	buffer = ptr2 
+	buffer = ptr2 			; Change this to use sp?
 		sta buffer 
 		stx buffer+1 
 
@@ -91,120 +91,125 @@
 ; void decodeRunLenRange(UInt8 *outData, const UInt8 *runLenData);
 .export _decodeRunLenRange 
 .proc _decodeRunLenRange
-	; Parameters:
-	; OLDCOL: number of bytes to skip before starting output.
-	; DELTAR: length of output requested
+	; * Decodes custom run-length encoded format data, skipping OLDCOL bytes
+	;   of output and writing out up to DELTAR bytes length.
+	; * The format is: 4 bits of the tile value, then 4 bits of the repeat count.
+	;   If those last 4 bits == $0F, add the value of the next 8 bits. 
+	;   The output is tile value written out repeat+1 times. 
+	; * Parameters:
+	;   OLDCOL: number of bytes to skip before starting output.
+	;   DELTAR: length of output requested
 	.import 	popptr1, popa
-	.importzp 	tmp1, tmp2, tmp3, tmp4, ptr1, ptr2, ptr3, ptr4
+	.importzp 	ptr1, ptr2, ptr3
+	.importzp 	tmp1
 
 .code
-	IN = ptr2				; $8A
-	sta IN
-	stx IN+1
+	src = ptr3 			; src is updated to point to next input byte
+	sta src
+	stx src+1
 
-	IN_LEN = tmp1			; $92
-	ldy #0
-	lda (IN),Y
-	sta IN_LEN
+	jsr inc_src_byte 	; discard length byte
 
-	IN_INDEX = tmp2			; $93
-	lda #1
-	sta IN_INDEX
-
-	OUT_LEN = DELTAR
-
-	SKIP = COLCRS
-	lda OLDCOL 
-	sta SKIP 
-
-	OUT = ptr1				; $8C
+	dst = ptr1  		; dst is updated to point to next output byte
 	jsr popptr1
 
-	OUT_INDEX = tmp4		; $95
-	lda #0
-	sta OUT_INDEX
+	skipRemaining = COLCRS 
+	lda OLDCOL
+	sta skipRemaining
 
-	TILE = ptr3				; $8E
-	REPEAT = ptr3+1			; $8F
-	
-	jmp while_input 	; 
+	tile = TMPCHR 		; store tile value in OS location
+
+	dstEnd = ptr2
+	clc
+	lda DELTAR
+	adc dst 
+	sta dstEnd
+	lda #0
+	adc dst+1
+	sta dstEnd+1
+
+	jmp next_input
 
 	loop_input:
-		ldy IN_INDEX	 	; A = in[in_index++]
-		lda (IN),Y
-		iny
+		ldy #0
+		lda (src),Y
+		jsr inc_src_byte 
 		tax
+		push_tile:
+			and #$F0			; tile = top 4 bits of A
+			lsr a 				; A = (A & 0xF0) >> 4 
+			lsr a
+			lsr a
+			lsr a
+			sta tile
+		get_repeat:
+			txa
+			and #$0F 			; repeat = bottom 4 bits of A
+			cmp #$0F
+			bne check_skip		; if repeat == $0F: add next byte
+		more_repeat:
+			clc				
+			adc (src),Y
+			jsr inc_src_byte 
+		check_skip:				
+			cmp skipRemaining	; if repeat < skip: fast-fwd past this block
+			bcs start_output
+		ffwd_skip:
+			eor #$FF			; subtract X from skipRemaining by using two's 
+			clc 				; complement to negate X and adding to skipRemaining
+			adc skipRemaining
+			sta skipRemaining
+			jmp next_input
+		start_output:
+			tax  				; put repeat on X
+			inx
+			jmp next_output
+		loop_output:
+			jsr output_byte
+			dex 
+		next_output:
+			cpx #0
+			beq next_input
+			lda dst  			; if dst == dstEnd: return
+			cmp dstEnd
+			bne @skip_msb
+				lda dst+1
+				cmp dstEnd+1
+				beq return 
+			@skip_msb:
+			jmp loop_output
 
-		and #$F0			; tile = top 4 bits of A
-		lsr a
-		lsr a
-		lsr a
-		lsr a
-		sta TILE
-
-		txa
-		and #$0F 			; repeat = bottom 4 bits of A
-		cmp #$0F
-		bne store_repeat 	; if repeat=$F, get add next byte
-
-		clc
-		adc (IN),Y			; repeat += in[in_index++]
-		iny
-
-	store_repeat:
-		clc
-		adc #1
-		sta REPEAT
-		sty IN_INDEX
-		
-		ldy OUT_INDEX		; use Y as out_index within the output loop
-		jmp while_output
-
-	loop_output:
-		lda SKIP			; if skip >= repeat
-		cmp REPEAT
-		bcc check_skip
-		
-		sbc REPEAT 			; fast-forward past skipped bytes
-		sta SKIP
-		jmp while_input		; and continue to next input byte
-
-	check_skip:
-		dec REPEAT			; repeat -= 1
-
-		ldx SKIP			; if skip > 0
-		beq output_byte
-		dex					; skip -= 1
-		stx SKIP
-		jmp while_output
-
-	output_byte:
-		lda TILE
-		sta (OUT),Y
-		iny
-
-	while_output: 			; while (repeat > 0 && outIndex < end)
-		lda REPEAT
-		beq while_input
-		
-		cpy OUT_LEN			; Y = out_index
-		bcs while_input
-
-		jmp loop_output
-
-	while_input:			; while (in_index < in_len && out_index < out_len)
-		sty OUT_INDEX		; save Y in out_index
-
-		lda IN_INDEX		; check in_index < in_len
-		cmp IN_LEN
-		bcs return
-
-		lda OUT_INDEX		; check out_index < out_len
-		cmp OUT_LEN
-		bcs return
-
-		jmp loop_input
-
+	next_input:				; while (dst != dstEnd)
+		lda dst 
+		cmp dstEnd
+		bne loop_input
+		lda dst+1
+		cmp dstEnd+1
+		bne loop_input
 	return:
 		rts
+
+	inc_src_byte:
+		inc src 
+		bne @skip_msb
+			inc src+1
+		@skip_msb:
+		rts 
+
+	output_byte:
+		ldy skipRemaining 
+		beq put_dst_byte
+		dec_skipRemaining:
+			dey
+			sty skipRemaining
+			rts 
+		put_dst_byte:
+			ldy #0
+			lda tile
+			sta (dst),Y
+			inc dst 
+			bne @skip_msb
+				inc dst+1
+			@skip_msb:
+			rts 
 .endproc
